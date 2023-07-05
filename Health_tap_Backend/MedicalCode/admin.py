4b89bb0cdf63f7dca2ea24f78432a516a581c7c1
omaramgad1 <omaramgad89@gmail.com>
2023-07-05 11:58:33 +0300

    modify admin panel for medical code

--- Health_tap_Backend/MedicalCode/admin.py
+++ Health_tap_Backend/MedicalCode/admin.py
@@ -2,4 +2,13 @@ from django.contrib import admin
 from MedicalCode.models import MedicalEditCode
 
 
-admin.site.register(MedicalEditCode)
+class MedicalEditCodeAdmin(admin.ModelAdmin):
+    list_display = ('patient', 'code', 'appointment',
+                    'status', 'created_at', 'expired_at')
+    list_filter = ('status',)
+    search_fields = ('patient__name',)
+    ordering = ('-created_at',)
+    readonly_fields = ('code', 'created_at', 'expired_at')
+
+
+admin.site.register(MedicalEditCode, MedicalEditCodeAdmin)
